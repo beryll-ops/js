7efa1c3a3b22003986d6930d3ddab4d7bb4cacd3
beryll-ops <75722271+beryll-ops@users.noreply.github.com>
2021-09-22 12:33:44 +0200

    1 test

---
+++ hello world.js
@@ -0,0 +1,3 @@
+<script type="text/javascript">
+document.write("HELLO WORLD");
+</script>
---
+++ js1.js
@@ -0,0 +1,5 @@
+<script type="text/javascript">
+document.write("to jest kolejny skrypt");
+var x=69;
+document.write(x);
+</script>
